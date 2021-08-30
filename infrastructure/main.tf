b8a001914e22e841429237aa9ddd64ff881cddf5
amr205 <alejandromedinareyesmx@gmail.com>
2021-08-30 16:21:37 -0600

    finalizacion del workflow

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -3,7 +3,7 @@ terraform {
     organization = "amr205"
 
     workspaces {
-      name = "WagtailApache"
+      name = "ApacheWagtail"
     }
   }
   required_providers {
@@ -19,16 +19,26 @@ provider "azurerm" {
   features {}
 }
 
+
+
 resource "azurerm_resource_group" "wagtail-apache" {
-  name     = "wagtail-apache-rg"
+  name     = "wagtail-apache-resources"
   location = "West US"
 }
 
-resource "azurerm_virtual_network" "wagtail-apache" {
-  name                = "wagtail-apache-vm"
-  address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.wagtail-apache.location
+resource "azurerm_public_ip" "wagtail-apache" {
+  name                = "wagtailPublicPublic"
   resource_group_name = azurerm_resource_group.wagtail-apache.name
+  location            = azurerm_resource_group.wagtail-apache.location
+  allocation_method   = "Dynamic"
+}
+
+resource "azurerm_virtual_network" "wagtail-apache" {
+  name                 = "wagtail-apache-network"
+  address_space        = ["10.0.0.0/16"]
+  location             = azurerm_resource_group.wagtail-apache.location
+  resource_group_name  = azurerm_resource_group.wagtail-apache.name
+
 }
 
 resource "azurerm_subnet" "wagtail-apache" {
@@ -36,13 +46,7 @@ resource "azurerm_subnet" "wagtail-apache" {
   resource_group_name  = azurerm_resource_group.wagtail-apache.name
   virtual_network_name = azurerm_virtual_network.wagtail-apache.name
   address_prefixes     = ["10.0.2.0/24"]
-}
 
-resource "azurerm_public_ip" "wagtail-apache" {
-  name                = "wagtail-apache-publicip"
-  resource_group_name = azurerm_resource_group.wagtail-apache.name
-  location            = azurerm_resource_group.wagtail-apache.location
-  allocation_method   = "Dynamic"
 }
 
 resource "azurerm_network_interface" "wagtail-apache" {
@@ -54,7 +58,7 @@ resource "azurerm_network_interface" "wagtail-apache" {
     name                          = "internal"
     subnet_id                     = azurerm_subnet.wagtail-apache.id
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.wagtail-apache.id
+    public_ip_address_id = azurerm_public_ip.wagtail-apache.id
   }
 }
 
@@ -70,7 +74,7 @@ resource "azurerm_linux_virtual_machine" "wagtail-apache" {
 
   admin_ssh_key {
     username   = "azureuser"
-    public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDZXZOzD7Lmkr8hEiCtUmR07wTJgTlEfUmRQ1k/KpbI6WG5WeYCaq/zJAw9F4IiI6XcdmXLvul6a00AtFiCKGj+XudIw9z3i2ha0fOlh1un5C6q7XUh9X+EFKw0Nje4rX6J86gUyNJxzXS/Zmae9nmi/Rz3UADana7mg+lRnuVMnayGvAR+gd5Ulb8ebBOBW2bc9WgGbZMHJVS6pDSgMH6XhQOmQh0QhlyKOqKr0QIId7zhELzE2IpJjXX6dF9jehrE6ycW0I8HMIVt31oZANL4S0aEC2eSIBabunlbhkpQ38xU8yPq6Wyzz/kYtLVfa57c3yiT7uiineq3q/u0eje+xmm1geo1aaSME89hTvrBTyY0XVKL+J5/dprbNHDTCN3joXX6JBlLbgRPVjPv8Bq+XU8MD4HVSlD033UD8uewpuFDT80R80tAFBsoBzwDZwTggzJ4hCQfFwMsBm+9R0jJ4Ny3AUfhxpVybSLwUpUAiLbnapfEpKrSE7eZAEH3aWc= amr205@amr205-Lenovo-Y50-70"
+    public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCyQNwh6kZEG2VTp35+C5J6/RDv2rS9dqNcAKvi6ab7ys4nfeUs+aelIHv6iXW49CJIF7ThmRgxJbVSqTaHKKfZV5B1yrzosEDeqlWmIp9b3tixYoOWpRpNZJmShwnxcTQMz8V4SAw2aRq6VaC7qqXiXiY85fqmsQ4Vpko9TdGgrfKY4NddR73mR7v9uuBrlnzucjq85iSVBR7pUQbikxMwWr/moV9PiYzsMi5ivoAQi+3lpSGLQZyIc6NVgY/EenhbvR+mTwHs7fYEyzCb+zIwCtLK3cZ2xpDO+LYPfIqsdxtVkF6Zr++EHkKiR9Jkta2rNEkbBXoL0HcPnMwJtcCIyd8eqePL9PJ4vRGxcnpRcz4DqH2EtrC6hmrNvx4v32Os0fji7xzD4bwhOER3b5LmIvo0VmMbjopHqXHLcZkrszvkrXwC4zrHO0mPnZTyw7iCEGH+1dfuTnYGdLby/2sSP4y2aSbJR7URsl/8xB+fI0eDa0las5vk2pdiY4vrjpU= amr205@amr205-Lenovo-Y50-70"
   }
 
   os_disk {
@@ -86,6 +90,7 @@ resource "azurerm_linux_virtual_machine" "wagtail-apache" {
   }
 }
 
-output "serverip" {
+output "server_ip" {
   value = azurerm_linux_virtual_machine.wagtail-apache.public_ip_address
 }
+
