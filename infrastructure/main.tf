e53304f8742690bc5bedd88ce31243490f831fb3
amr205 <alejandromedinareyesmx@gmail.com>
2022-09-10 22:27:14 -0600

    Update main.tf

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -51,8 +51,8 @@ resource "azurerm_subnet" "wagtail-apache" {
 
 resource "azurerm_mssql_server" "wagtail-db-server" {
   name                         = "wagtaildbserver"
-  resource_group_name          = azurerm_resource_group.example.name
-  location                     = azurerm_resource_group.example.location
+  resource_group_name          = azurerm_resource_group.wagtail-apache.name
+  location                     = azurerm_resource_group.wagtail-apache.location
   version                      = "12.0"
   administrator_login          = "4dm1n157r470r"
   administrator_login_password = "4-v3ry-53cr37-p455w0rd"
